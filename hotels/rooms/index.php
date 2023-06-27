db152385758a58c282dac770c49feb7e9de73365
NINDP <127078427+NINDP@users.noreply.github.com>
2023-06-27 15:55:03 +0300

    Update index.php

--- hotels/rooms/index.php
+++ hotels/rooms/index.php
@@ -1,17 +1,17 @@
 <?php
 function viewRooms()
 {
-    $o = file_get_contents("../../php.json");
-    $a = json_decode($o, true);
-    $keys = array_keys(array_column($a["rooms"], 'hotel'), $_GET['idHotel']);
+    $json = file_get_contents("../../php.json");
+    $json = json_decode($json, true);
+    $keys = array_keys(array_column($json["rooms"], 'hotel'), $_GET['idHotel']);
     if ($keys or $keys === 0) {
         foreach ($keys as $key) {
-            $result[] = $a["rooms"][$key];
+            $result[] = $json["rooms"][$key];
         }
     } else {
         die(json_encode([
             'status' => false,
-            'rooms' => $a["rooms"]
+            'rooms' => $json["rooms"]
         ]));
     }
     die(json_encode([
