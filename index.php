c9aeb01781667b26d7ebb1d7b3bbac6dc6beddf0
NINDP <127078427+NINDP@users.noreply.github.com>
2023-06-27 15:58:55 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,73 +1,73 @@
 <?php
 function addClient($name, $surname, $phoneNumber, $email)
 {
-    $o = file_get_contents('php.json');
-    $a = json_decode($o, true);
-    $a['clients'][] = [
-        'id' => count($a['clients']) + 1,
+    $json = file_get_contents('php.json');
+    $json = json_decode($json, true);
+    $json['clients'][] = [
+        'id' => count($json['clients']) + 1,
         'name' => $name,
         'surname' => $surname,
         "phoneNumber" => $phoneNumber,
         "email" => $email
     ];
-    $newJsonString = json_encode($a);
+    $newJsonString = json_encode($json);
     file_put_contents('php.json', $newJsonString);
     die(json_encode([
         'status' => true,
-        'clients' => $a["clients"]
+        'clients' => $json["clients"]
     ]));
 }
 
 
 function addOrders($idClient, $idRoom, $dateDeparture)
 {
-    $o = file_get_contents('php.json');
-    $a = json_decode($o, true);
-    $keyRoom = array_search($idRoom, array_column($a["rooms"], 'id'));
-    if ($a["rooms"][$keyRoom]["isVacant"] === false) {
-        $a["rooms"][$keyRoom]["isVacant"] = true;
-        $a['orders'][] = [
-            'id' => count($a['orders']) + 1,
+    $json = file_get_contents('php.json');
+    $json = json_decode($json, true);
+    $keyRoom = array_search($idRoom, array_column($json["rooms"], 'id'));
+    if ($json["rooms"][$keyRoom]["isVacant"] === false) {
+        $json["rooms"][$keyRoom]["isVacant"] = true;
+        $json['orders'][] = [
+            'id' => count($json['orders']) + 1,
             'arrivalDate' => date("d.m.Y"),
             'departureDate' => $dateDeparture,
-            "room" => $a["rooms"][$keyRoom]["id"],
-            "cost" => $a["rooms"][$keyRoom]["cost"]
+            "room" => $json["rooms"][$keyRoom]["id"],
+            "cost" => $json["rooms"][$keyRoom]["cost"]
         ];
-        $newJsonString = json_encode($a);
+        $newJsonString = json_encode($json);
         file_put_contents('php.json', $newJsonString);
         die(json_encode([
             'status' => true,
-            'rooms' => $a["rooms"],
+            'rooms' => $json["rooms"],
         ]));
 
     } else {
         die(json_encode([
             'status' => false,
-            'rooms' => $a["rooms"],
+            'rooms' => $json["rooms"],
         ]));
     }
 }
 
 function changeVacant()
 {
-    $o = file_get_contents('./php.json');
-    $a = json_decode($o, true);
-    $keyOrder = array_search(date("d.m.Y"), array_column($a["orders"], 'departureDate'));
+    $json = file_get_contents('./php.json');
+    $json = json_decode($json, true);
+    $keyOrder = array_search(date("d.m.Y"), array_column($json["orders"], 'departureDate'));
     if ($keyOrder or $keyOrder === 0) {
-        $numberRooms = $a["orders"][$keyOrder]["room"];
-        $keyRoom = array_search($numberRooms, array_column($a["rooms"], 'id'));
-        $a["rooms"][$keyRoom]["isVacant"] = false;
+        $numberRooms = $json["orders"][$keyOrder]["room"];
+        $keyRoom = array_search($numberRooms, array_column($json["rooms"], 'id'));
+        $json["rooms"][$keyRoom]["isVacant"] = false;
     } else {
         die(json_encode([
             'status' => false,
-            'rooms' => $a["rooms"],
+            'rooms' => $json["rooms"],
         ]));
     }
-    $newJsonString = json_encode($a);
+    $newJsonString = json_encode($json);
     file_put_contents('php.json', $newJsonString);
     die(json_encode([
         'status' => true,
-        'rooms' => $a["rooms"],
+        'rooms' => $json["rooms"],
     ]));
 }
 
