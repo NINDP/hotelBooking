aa06e729091a4ee3fce0d0bb34f26ff966b758ef
NINDP <127078427+NINDP@users.noreply.github.com>
2023-06-27 15:55:32 +0300

    Update index.php

--- hotels/index.php
+++ hotels/index.php
@@ -1,18 +1,18 @@
 <?php
 function viewHotel()
 {
-    $o = file_get_contents("../php.json");
-    $a = json_decode($o, true);
-    $place = array_search($_GET['id'], array_column($a["hotels"], 'id'));
+    $json = file_get_contents("../php.json");
+    $json = json_decode($o, true);
+    $place = array_search($_GET['id'], array_column($json["hotels"], 'id'));
     if ($place or $place === 0) {
         die(json_encode([
             'status' => true,
-            'hotel' => $a["hotels"][$place]
+            'hotel' => $json["hotels"][$place]
         ]));
     } else {
         die(json_encode([
             'status' => false,
-            'hotels' => $a["hotels"]
+            'hotels' => $json["hotels"]
         ]));
     }
 }
